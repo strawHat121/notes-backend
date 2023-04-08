335f7a34511380537dc872d37dbd0f652ff66437
Kinjalk <kinjalkbajpai@gmail.com>
2023-04-08 17:40:22 +0530

    Added edit and delete routes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const Note = require("./models/notes")
+require("dotenv").config()
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.get("/", (req, res) => {
@@ -21,6 +23,16 @@ app.get("/allnotes", async (req, res) => {
     }
 })
 
+app.get("/allNotes/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const note = await Note.findById(id);
+        res.status(200).json(note);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 app.post("/create", async (req, res) => {
     try {
         const note = await Note.create(req.body);
@@ -31,8 +43,36 @@ app.post("/create", async (req, res) => {
     }
 })
 
+app.put('/editnote/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const note = await Note.findByIdAndUpdate(id, req.body)
+        if (!note) {
+            return res.status(404).json({ message: `Cannot find any product with the id ${id}` });
+        }
+        const updatedNote = await Note.findById(id)
+        res.status(200).json(updatedNote)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: err.message })
+    }
+})
 
-mongoose.connect('mongodb+srv://kinjalkbajpai:kinjalkbajpai@cluster0.3g8okrk.mongodb.net/?retryWrites=true&w=majority').then(() => {
+app.delete('/deletenote/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const note = await Note.findByIdAndDelete(id)
+        if (!note) {
+            return res.status(404).json({ message: `Cannot find any product with the id ${id}` });
+        }
+        res.status(200).json(note)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: err.message })
+    }
+})
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connected to mongo")
     app.listen(3000, () => {
         console.log('Node API is running')
